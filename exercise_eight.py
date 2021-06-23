af757e0fc1ebeeceaee1a47964a54f0893bdf15e
PerovaKate <85638736+PerovaKate@users.noreply.github.com>
2021-06-23 23:47:05 +0300

    Initial commit

---
+++ exercise_eight.py
@@ -0,0 +1,10 @@
+name = 'Каша'
+annotation = 'Полезный завтрак из крупы'
+cooking_time = '20 минут'
+desc = 'Возьмите 100 грамм крупы, залейте кипятком и подождите 20 минут'
+steps = '2 этапа приготовления'
+ingredients = '3 ингредиента (соль, крупа, вода)'
+recipe= [name,annotation,cooking_time,desc,steps,ingredients]
+print(recipe)
+new_recipe = recipe[:3]
+print(new_recipe)
